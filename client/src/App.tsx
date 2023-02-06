ed108839f150a6d8bec3347487073af4d3249ffc
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-06 15:30:54 +0400

    import after new line required and sort

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 function App(): JSX.Element {
   return (
     <div className="App">
@@ -22,5 +23,4 @@ function App(): JSX.Element {
     </div>
   );
 }
-
 export default App;
