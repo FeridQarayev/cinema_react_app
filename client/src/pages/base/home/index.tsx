0c02345aa37fa17904ccbd86fe05e29fb70bd76c
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-08 18:41:38 +0400

    slider container add body and style

--- client/src/pages/base/home/index.tsx
+++ client/src/pages/base/home/index.tsx
@@ -1,5 +1,6 @@
 import useEmblaCarousel from 'embla-carousel-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import slideImg2 from '../../../images/slider/banner-1.jpg';
 import slideImg1 from '../../../images/slider/banner-2.jpg';
 import slideImg3 from '../../../images/slider/banner-3.png';
@@ -8,20 +9,66 @@ import styled from './home.module.scss';
 function Home(): JSX.Element {
   const [emblaRef] = useEmblaCarousel();
   return (
-    <div className={styled.embla} ref={emblaRef}>
-      <div className={styled.embla__container}>
-        <div
-          style={{ backgroundImage: `url(${String(slideImg1)})` }}
-          className={styled.embla__slide}
-        ></div>
-        <div
-          style={{ backgroundImage: `url(${String(slideImg2)})` }}
-          className={styled.embla__slide}
-        ></div>
-        <div
-          style={{ backgroundImage: `url(${String(slideImg3)})` }}
-          className={styled.embla__slide}
-        ></div>
+    <div className={styled.home}>
+      <div className={styled.home__slider} ref={emblaRef}>
+        <div className={styled.home__slider__container}>
+          <div
+            style={{ backgroundImage: `url(${String(slideImg1)})` }}
+            className={styled.home__slider__container__slide}
+          >
+            <div className={styled.home__slider__container__slide__body}>
+              <div className={styled.home__slider__container__slide__body__row}>
+                <div className={styled.home__slider__container__slide__body__row__col}>
+                  <div className={styled.home__slider__container__slide__body__row__col__text}>
+                    <h2>Deadpool: No Good Deed</h2>
+                    <div
+                      className={styled.home__slider__container__slide__body__row__col__text__cast}
+                    >
+                      <h6>
+                        <strong>Cast:</strong>
+                      </h6>
+                      <p>Ryan Reynolds (Deadpool)</p>
+                      <p>Morena Baccarin (Vanessa)</p>
+                    </div>
+                    <ul>
+                      <li>
+                        <span>HD</span>
+                      </li>
+                      <li>
+                        <strong>Rated</strong>
+                        &nbsp; 8.5
+                      </li>
+                      <li>
+                        <strong>Genre:</strong>
+                        &nbsp; Horror, Comedy, Action, Sci-Fi.
+                      </li>
+                    </ul>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+                      when an unknown printer took a galley of type and scrambled it to make a type
+                      specimen book.
+                    </p>
+                    <div
+                      className={styled.home__slider__container__slide__body__row__col__text__btn}
+                    >
+                      <Link to={'home'}>Watch</Link>
+                      <Link to={'home'}>+ Add To Watchlist</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${String(slideImg2)})` }}
+            className={styled.home__slider__container__slide}
+          ></div>
+          <div
+            style={{ backgroundImage: `url(${String(slideImg3)})` }}
+            className={styled.home__slider__container__slide}
+          ></div>
+        </div>
       </div>
     </div>
   );
