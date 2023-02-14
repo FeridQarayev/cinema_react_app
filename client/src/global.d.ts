6e1ea8d8546251959d8b0eae86a0cd05e456c0e5
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-15 00:49:44 +0400

    base about us up section html

--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -3,3 +3,4 @@ declare module '*.scss';
 declare module '*.jpg';
 declare module '*.png';
 declare module '*.svg';
+declare module '*.mp4';
--- client/src/pages/base/aboutus/index.tsx
+++ client/src/pages/base/aboutus/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import record from '../../../images/aboutus/maxresdefault-11.jpg';
+import video from '../../../videos/Free HD Stock Film 8mm Projector broll.mp4';
 import styled from './aboutus.module.scss';
 
 function AboutUs(): JSX.Element {
-  return <div>AboutUs</div>;
+  return (
+    <div className={styled.about}>
+      <section className={styled.about__up}>
+        <div className={styled.about__up__back}>
+          <img src={record} alt="cinema" />
+          <div className={styled.about__up__back__video}>
+            <iframe
+              src={video}
+              frameBorder="0"
+              title="Free HD Stock Film 8mm Projector broll"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+          </div>
+        </div>
+        <div className={styled.about__up__color}></div>
+        <div className={styled.about__up__body}>
+          <div className={styled.about__up__body__container}>
+            <div className={styled.about__up__body__container__text}>
+              <h2>About Us</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default AboutUs;
