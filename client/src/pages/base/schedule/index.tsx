0c266501783614ff12507572be3cc99747504861
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-17 12:22:17 +0400

    add background img schedule

--- client/src/pages/base/schedule/index.tsx
+++ client/src/pages/base/schedule/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import backimg from '../../../images/schedule/bg.jpg';
 import topRight from '../../../images/schedule/bobine.png';
 import bottomLeft from '../../../images/schedule/bottom-left.png';
 import bottomRight from '../../../images/schedule/bottom-right.png';
@@ -9,55 +10,60 @@ import styled from './schedule.module.scss';
 function Schedule(): JSX.Element {
   return (
     <div className={styled.schedule}>
-      <section className={styled.schedule__up}>
-        <div className={styled.schedule__up__container}>
-          <div className={styled.schedule__up__container__content}>
-            <div className={styled.schedule__up__container__content__col}>
-              <div className={styled.schedule__up__container__content__col__def}>
-                <div className={styled.schedule__up__container__content__col__def__inner}>
-                  <div className={styled.schedule__up__container__content__col__def__inner__wrap}>
-                    <div className={styled.schedule__up__container__content__col__def__inner__wrap__force}>
-                      <div className={styled.schedule__up__container__content__col__def__inner__wrap__force__rev}>
-                        <article>
-                          <div className={styled.slides}>
-                            <div className={styled.slides__slide}>
-                              <div className={styled.slides__slide__text}>
-                                <p>A Video WordPress Theme </p>
-                              </div>
-                              <div className={styled.slides__slide__middledown}>
-                                <div>
-                                  <img src={middleDown} alt="galesses" />
+      <section>
+        <div className={styled.schedule__img}>
+          <img src={backimg} alt="back" />
+        </div>
+        <div className={styled.schedule__up}>
+          <div className={styled.schedule__up__container}>
+            <div className={styled.schedule__up__container__content}>
+              <div className={styled.schedule__up__container__content__col}>
+                <div className={styled.schedule__up__container__content__col__def}>
+                  <div className={styled.schedule__up__container__content__col__def__inner}>
+                    <div className={styled.schedule__up__container__content__col__def__inner__wrap}>
+                      <div className={styled.schedule__up__container__content__col__def__inner__wrap__force}>
+                        <div className={styled.schedule__up__container__content__col__def__inner__wrap__force__rev}>
+                          <article>
+                            <div className={styled.slides}>
+                              <div className={styled.slides__slide}>
+                                <div className={styled.slides__slide__text}>
+                                  <h1>A Video WordPress Theme </h1>
                                 </div>
-                              </div>
-                              <div className={styled.slides__slide__topleft}>
-                                <div>
+                                <div className={styled.slides__slide__middledown}>
                                   <div>
-                                    <img src={topLeft} alt="popcorn" />
+                                    <img src={middleDown} alt="galesses" />
                                   </div>
                                 </div>
-                              </div>
-                              <div className={styled.slides__slide__bottomright}>
-                                <div>
+                                <div className={styled.slides__slide__topleft}>
                                   <div>
-                                    <img src={bottomRight} alt="popcorn" />
+                                    <div>
+                                      <img src={topLeft} alt="popcorn" />
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                              <div className={styled.slides__slide__bottomleft}>
-                                <div>
-                                  <img src={bottomLeft} alt="popcorn" />
+                                <div className={styled.slides__slide__bottomright}>
+                                  <div>
+                                    <div>
+                                      <img src={bottomRight} alt="popcorn" />
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                              <div className={styled.slides__slide__topright}>
-                                <div>
-                                  <img src={topRight} alt="cola" />
+                                <div className={styled.slides__slide__bottomleft}>
+                                  <div>
+                                    <img src={bottomLeft} alt="popcorn" />
+                                  </div>
+                                </div>
+                                <div className={styled.slides__slide__topright}>
+                                  <div>
+                                    <img src={topRight} alt="cola" />
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
-                        </article>
+                          </article>
+                        </div>
+                        <div className={styled.schedule__up__container__content__col__def__inner__wrap__force__fw}></div>
                       </div>
-                      <div className={styled.schedule__up__container__content__col__def__inner__wrap__force__fw}></div>
                     </div>
                   </div>
                 </div>
