335f90fb83e5b06de5b2425a7868bc341cdb1947
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-25 13:23:34 +0400

    session model

---
+++ server/models/session.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const sessionSchema = new mongoose.Schema({
+  movie: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Movie",
+  },
+  hall: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hall",
+  },
+  date: { type: Date },
+  price: { type: Number, default: 0 },
+  formats: {
+    d2: { type: Boolean, default: false },
+    d3: { type: Boolean, default: false },
+    d4: { type: Boolean, default: false },
+  },
+  language: { type: String, default: null },
+  reserved: [
+    {
+      col: { type: Number },
+      row: { type: Number },
+    },
+  ],
+});
+
+module.exports = mongoose.model("Session", sessionSchema);
