792e45820a2c7197569c74c6428759a75c4d514c
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-25 23:23:56 +0400

    navbar left styled

--- client/src/components/admin/navbar/index.tsx
+++ client/src/components/admin/navbar/index.tsx
@@ -8,29 +8,28 @@ import styled from './navbar.module.scss';
 function Navbar(): JSX.Element {
   return (
     <nav className={styled.navbar} style={{ backgroundImage: `url(${String(backImg)})` }}>
-      <div className={styled.navbar__header}></div>
       <div className={styled.navbar__body}>
         <h4>My Cinema</h4>
         <ul className={styled.navbar__body__list}>
-          <li className={styled.navbar__body__list}>
+          <li className={styled.navbar__body__list__item}>
             <Link to={'#'}>
               <Gauge />
               <span>Dashboard</span>
             </Link>
           </li>
-          <li className={styled.navbar__body__list}>
+          <li className={styled.navbar__body__list__item}>
             <Link to={'#'}>
               <Cinema />
               <span>Cinema</span>
             </Link>
           </li>
-          <li className={styled.navbar__body__list}>
+          <li className={styled.navbar__body__list__item}>
             <Link to={'#'}>
               <Gauge />
               <span>Dashboard</span>
             </Link>
           </li>
-          <li className={styled.navbar__body__list}>
+          <li className={styled.navbar__body__list__item}>
             <Link to={'#'}>
               <Cinema />
               <span>Cinema</span>
