baedb08bcff7915d6b91376a6143c00fff1e65ac
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-26 22:25:38 +0400

    movie add imports

--- client/src/pages/admin/movie/index.tsx
+++ client/src/pages/admin/movie/index.tsx
@@ -1,4 +1,23 @@
-import React from 'react';
+import { Modal } from '@mui/material';
+import { Field, Form, Formik } from 'formik';
+import MaterialReactTable, { type MRT_ColumnDef } from 'material-react-table';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Toaster, toast } from 'react-hot-toast';
+import * as Yup from 'yup';
+import styled from './movie.module.scss';
+
+const CreateSchema = Yup.object().shape({
+  name: Yup.string().min(3, 'Too Short!').max(20, 'Too Long!').required('Required!'),
+  column: Yup.number().min(5).max(15).required('Required!'),
+  row: Yup.number().min(5).max(15).required('Required!'),
+  cinemaId: Yup.string().required('Required!'),
+});
+
+const UpdateSchema = Yup.object().shape({
+  name: Yup.string().min(3, 'Too Short!').max(20, 'Too Long!').required('Required!'),
+  column: Yup.number().min(5).max(15).required('Required!'),
+  row: Yup.number().min(5).max(15).required('Required!'),
+});
 
 function MovieAdmin(): JSX.Element {
   return <div>MovieAdmin</div>;
