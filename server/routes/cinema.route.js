1c4b9674ae2ef1504845bc29657b925e2a9baaae
FeridQarayev <tu7he6n9s@code.edu.az>
2023-02-27 21:35:59 +0400

    hall route add middleware

--- server/routes/cinema.route.js
+++ server/routes/cinema.route.js
@@ -2,7 +2,6 @@ const controller = require("../controllers/cinema.controller");
 const auth = require("../middlewares/auth");
 
 module.exports = function (app) {
-  //   app.post("/api/cinema", [auth.verifyToken, auth.isAdmin], controller.create);
   app.get("/api/cinema", controller.get);
 
   app.post("/api/cinema/id", controller.getById);
--- server/routes/hall.route.js
+++ server/routes/hall.route.js
@@ -1,13 +1,14 @@
 const controller = require("../controllers/hall.controller");
+const auth = require("../middlewares/auth");
 
 module.exports = function (app) {
   app.get("/api/hall", controller.get);
 
   app.post("/api/hall/id", controller.getById);
 
-  app.post("/api/hall", controller.create);
+  app.post("/api/hall", [auth.verifyToken, auth.isAdmin], controller.create);
 
-  app.put("/api/hall", controller.update);
+  app.put("/api/hall", [auth.verifyToken, auth.isAdmin], controller.update);
 
-  app.delete("/api/hall", controller.delete);
+  app.delete("/api/hall", [auth.verifyToken, auth.isAdmin], controller.delete);
 };
